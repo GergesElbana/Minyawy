1e668ddbd58628af9bf070a9241d28a551d2e2f9
Gerges Elbana <GergesElbana>
2020-12-02 15:02:20 +0200

    Admin

---
+++ app/src/main/java/com/example/minyawy/Admin/RestaurantDescriptionAdmin.java
@@ -0,0 +1,15 @@
+package com.example.minyawy.Admin;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.minyawy.R;
+
+public class RestaurantDescriptionAdmin extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_restaurant_description_admin);
+    }
+}
